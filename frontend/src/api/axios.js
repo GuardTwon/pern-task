1bf7b2f04aa15e6ae552d34fbe8fbf75d78c268c
GuardTwon <76417327+GuardTwon@users.noreply.github.com>
2023-08-07 14:58:20 -0600

    ready for deply v1

--- frontend/src/api/axios.js
+++ frontend/src/api/axios.js
@@ -1,6 +1,9 @@
 import axios from 'axios';
 
+const baseURL=import.meta.env.VITE_BACKEND||"http://127.0.0.1:3000/api"
+
 const client =axios.create({
-    baseURL:'http://127.0.0.1:3000/api',withCredentials:true
+    baseURL:baseURL,
+    withCredentials:true
 })
 export default client
