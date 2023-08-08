d64a3f4db22e67a2a685ca5edd560ff8900faea3
GuardTwon <76417327+GuardTwon@users.noreply.github.com>
2023-08-07 19:22:01 -0600

    ready for deploy vr 2

--- frontend/src/api/axios.js
+++ frontend/src/api/axios.js
@@ -3,7 +3,7 @@ import axios from 'axios';
 const baseURL=import.meta.env.VITE_BACKEND||"http://127.0.0.1:3000/api"
 
 const client =axios.create({
-    baseURL:baseURL,
+    baseURL,
     withCredentials:true
 })
 export default client
